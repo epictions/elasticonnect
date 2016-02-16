f7852ef63680be41818a9b3e6c5f962b010482dc
Sambasiva Suda <ssuda@epictions.com>
2016-02-16 13:10:35 +0530

    update fix

--- src/es-update.js
+++ src/es-update.js
@@ -6,7 +6,7 @@ module.exports = function(resp, commander) {
   var docs =  _.map(resp.hits.hits, function(hit) {
     return [
       {update: _.pick(hit, '_index', '_type', '_id')},
-      {doc: commander.extractDocument(hit), detect_noop: true, doc_as_upsert: true}
+      {doc: hit._source, detect_noop: true, doc_as_upsert: true}
     ]
   })
 
